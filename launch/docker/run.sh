af30879a33e82ac57c3fd65ac74046b0ea9dcf5e
lkr <2793158317@qq.com>
2023-06-20 10:27:43 -0700

    docker

---
+++ launch/docker/run.sh
@@ -0,0 +1,7 @@
+docker run -i -d --rm --runtime=nvidia --name dac \
+            -e NVIDIA_DRIVER_CAPABILITIES=all \
+                    -v ~/:/root/ \
+                    gitlab-registry.nrp-nautilus.io/openhonor/diffusion-policies-for-offline-rl
+                                    
+docker exec -it dac bash
+# -e QT_X11_NO_MITSHM=1 -e XAUTHORITY 
