48803efbc4f4f4dcb4a28347553396f79420a75d
lkr <2793158317@qq.com>
2023-06-21 10:52:50 -0700

    algorithm

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import random
 
 index_range = range(1, 11)
 config_dir = "configs/"
@@ -23,7 +24,8 @@ config_dir = "configs/"
 #     os.system(command)
 
 discount2 = [0.9, 0.99, 1.0]  # 0.999 is the best
-coef = [0.5, 0.75, 1.25, 1.5]  # 1.0 is the best
+# coef = [0.5, 0.75, 1.25, 1.5]  # 1.0 is the best
+coef = [1.25, 1.5] 
 T = [2, 3, 4]  # 1 is the best
 compute_consistency = [True, False]  # True is the best
 algo = ['bc', 'ql', ]
@@ -37,48 +39,137 @@ algo = ['bc', 'ql', ]
 #     "T": 1,
 # }
 
-filename = f"halfcheetah-best.yaml"
-config = {
-    "discount2": 0.999,
-    "coef": 1.0,
-    "lr_decay": False,
-    "early_stop": False,
-    "seed": 0,
-    "T": 1,
-}
-filename = os.path.join(config_dir, filename)
-with open(filename, "w") as file:
-    yaml.dump(config, file)
-command = f"python launch/remote_run.py --job_name dac-best main.py --config {filename} --run"
-os.system("git pull origin master")
-os.system("git add .")
-os.system(f"git commit -m '{command}''")
-os.system("git push origin master'")
-os.system(command)
+# filename = f"halfcheetah-best.yaml"
+# config = {
+#     "discount2": 0.999,
+#     "coef": 1.0,
+#     "lr_decay": False,
+#     "early_stop": False,
+#     "seed": 0,
+#     "T": 1,
+# }
+# filename = os.path.join(config_dir, filename)
+# with open(filename, "w") as file:
+#     yaml.dump(config, file)
+# command = f"python launch/remote_run.py --job_name dac-best main.py --config {filename} --run"
+# os.system("git pull origin master")
+# os.system("git add .")
+# os.system(f"git commit -m '{command}''")
+# os.system("git push origin master'")
+# os.system(command)
 
-# discount2
-for dis in discount2:
-    filename = f"halfcheetah-discount2{dis}.yaml"
-    config = {
-        "discount2": dis,
-        "coef": 1.0,
-        "lr_decay": False,
-        "early_stop": False,
-        "seed": 0,
-        "T": 1,
-    }
-    filename = os.path.join(config_dir, filename)
-    with open(filename, "w") as file:
-        yaml.dump(config, file)
+# # discount2
+# for dis in discount2:
+#     filename = f"halfcheetah-discount2{dis}.yaml"
+#     config = {
+#         "discount2": dis,
+#         "coef": 1.0,
+#         "lr_decay": False,
+#         "early_stop": False,
+#         "seed": 0,
+#         "T": 1,
+#     }
+#     filename = os.path.join(config_dir, filename)
+#     with open(filename, "w") as file:
+#         yaml.dump(config, file)
 
-    command = f"python launch/remote_run.py --job_name dac-discount2{dis} main.py --config {filename} --run"
-    os.system("git pull origin master")
-    os.system("git add .")
-    os.system(f"git commit -m '{command}''")
-    os.system("git push origin master'")
-    os.system(command)
+#     command = f"python launch/remote_run.py --job_name dac-discount2{dis} main.py --config {filename} --run"
+#     os.system("git pull origin master")
+#     os.system("git add .")
+#     os.system(f"git commit -m '{command}''")
+#     os.system("git push origin master'")
+#     os.system(command)
 
 # coef
+# for c in coef:
+#     filename = f"halfcheetah-coef{c}.yaml"
+#     config = {
+#         "discount2": 0.999,
+#         "coef": c,
+#         "lr_decay": False,
+#         "early_stop": False,
+#         "seed": 0,
+#         "T": 1,
+#         "device": random.randint(0, 8)
+#     }
+#     filename = os.path.join(config_dir, filename)
+#     with open(filename, "w") as file:
+#         yaml.dump(config, file)
+
+#     command = f"python launch/remote_run.py --job_name dac-coef{c} main.py --config {filename} --run"
+#     os.system("git pull origin master")
+#     os.system("git add .")
+#     os.system(f"git commit -m '{command}''")
+#     os.system("git push origin master'")
+#     os.system(command)
+
+# # T
+# for t in T:
+#     filename = f"halfcheetah-T{t}.yaml"
+#     config = {
+#         "discount2": 0.999,
+#         "coef": 1.0,
+#         "lr_decay": False,
+#         "early_stop": False,
+#         "seed": 0,
+#         "T": t,
+#         "device": random.randint(0, 8)
+#     }
+#     filename = os.path.join(config_dir, filename)
+#     with open(filename, "w") as file:
+#         yaml.dump(config, file)
+
+#     command = f"python launch/remote_run.py --job_name dac-t{t} main.py --config {filename} --run"
+#     os.system("git pull origin master")
+#     os.system("git add .")
+#     os.system(f"git commit -m '{command}''")
+#     os.system("git push origin master'")
+#     os.system(command)
+
+
+# # compute_consistency
+# filename = f"halfcheetah-compute_consistency.yaml"
+# config = {
+#     "discount2": 0.999,
+#     "coef": 1.0,
+#     "lr_decay": False,
+#     "early_stop": False,
+#     "seed": 0,
+#     "T": 1,
+#     "compute_consistency": False,
+#     "device": random.randint(0, 8)
+# }
+# filename = os.path.join(config_dir, filename)
+# with open(filename, "w") as file:
+#     yaml.dump(config, file)
+# command = f"python launch/remote_run.py --job_name dac-compute_consistency main.py --config {filename} --run"
+# os.system("git pull origin master")
+# os.system("git add .")
+# os.system(f"git commit -m '{command}''")
+# os.system("git push origin master'")
+# os.system(command)
+
+# for al in algo:
+#     filename = f"halfcheetah-algo{al}.yaml"
+#     config = {
+#         "discount2": 0.999,
+#         "coef": 1.0,
+#         "lr_decay": False,
+#         "early_stop": False,
+#         "seed": 0,
+#         "T": 1,
+#         "algo": al,
+#         "device": random.randint(0, 8)
+#     }
+#     filename = os.path.join(config_dir, filename)   
+#     with open(filename, "w") as file:
+#         yaml.dump(config, file)
+#     command = f"python launch/remote_run.py --job_name dac-algo{al} main.py --config {filename} --run"
+#     os.system("git pull origin master")
+#     os.system("git add .")
+#     os.system(f"git commit -m '{command}''")
+#     os.system("git push origin master'")
+
 for c in coef:
     filename = f"halfcheetah-coef{c}.yaml"
     config = {
@@ -88,12 +179,13 @@ for c in coef:
         "early_stop": False,
         "seed": 0,
         "T": 1,
+        "device": random.randint(0, 8)
     }
     filename = os.path.join(config_dir, filename)
     with open(filename, "w") as file:
         yaml.dump(config, file)
 
-    command = f"python launch/remote_run.py --job_name dac-coef{c} main.py --config {filename} --run"
+    command = f"python main.py --config {filename}"
     os.system("git pull origin master")
     os.system("git add .")
     os.system(f"git commit -m '{command}''")
@@ -110,12 +202,13 @@ for t in T:
         "early_stop": False,
         "seed": 0,
         "T": t,
+        "device": random.randint(0, 8)
     }
     filename = os.path.join(config_dir, filename)
     with open(filename, "w") as file:
         yaml.dump(config, file)
 
-    command = f"python launch/remote_run.py --job_name dac-t{t} main.py --config {filename} --run"
+    command = f"python main.py --config {filename}"
     os.system("git pull origin master")
     os.system("git add .")
     os.system(f"git commit -m '{command}''")
@@ -133,11 +226,12 @@ config = {
     "seed": 0,
     "T": 1,
     "compute_consistency": False,
+    "device": random.randint(0, 8)
 }
 filename = os.path.join(config_dir, filename)
 with open(filename, "w") as file:
     yaml.dump(config, file)
-command = f"python launch/remote_run.py --job_name dac-compute_consistency main.py --config {filename} --run"
+command = f"python main.py --config {filename}"
 os.system("git pull origin master")
 os.system("git add .")
 os.system(f"git commit -m '{command}''")
@@ -154,11 +248,12 @@ for al in algo:
         "seed": 0,
         "T": 1,
         "algo": al,
+        "device": random.randint(0, 8)
     }
     filename = os.path.join(config_dir, filename)   
     with open(filename, "w") as file:
         yaml.dump(config, file)
-    command = f"python launch/remote_run.py --job_name dac-algo{al} main.py --config {filename} --run"
+    command = f"python main.py --config {filename}"
     os.system("git pull origin master")
     os.system("git add .")
     os.system(f"git commit -m '{command}''")
