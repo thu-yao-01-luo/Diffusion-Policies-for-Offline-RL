0bcf2f1a5619839953b45175f15c968c54b39023
lkr <2793158317@qq.com>
2023-06-20 19:16:14 -0700

    update

--- script.py
+++ script.py
@@ -4,18 +4,18 @@ import os
 index_range = range(1, 11)
 config_dir = "configs/"
 
-for index in index_range:
-    filename = f"halfcheetah-sota-seed{index}.yaml"
-    config = {
-        "discount2": 0.99,
-        "coef": 1.0,
-        "lr_decay": False,
-        "early_stop": False,
-        "seed": index,
-    }
-    filename = os.path.join(config_dir, filename)
-    with open(filename, "w") as file:
-        yaml.dump(config, file)
+# for index in index_range:
+#     filename = f"halfcheetah-sota-seed{index}.yaml"
+#     config = {
+#         "discount2": 0.99,
+#         "coef": 1.0,
+#         "lr_decay": False,
+#         "early_stop": False,
+#         "seed": index,
+#     }
+#     filename = os.path.join(config_dir, filename)
+#     with open(filename, "w") as file:
+#         yaml.dump(config, file)
 
-    command = f"python launch/remote_run.py --job_name dac-sota-seed{index} main.py --config {filename} --run"
-    os.system(command)
+#     command = f"python launch/remote_run.py --job_name dac-sota-seed{index} main.py --config {filename} --run"
+#     os.system(command)
