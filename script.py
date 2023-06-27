b411ae9a2349785b2fac901ca441cebed739db45
lkr <2793158317@qq.com>
2023-06-26 17:11:29 -0700

    update

--- script.py
+++ script.py
@@ -711,9 +711,9 @@ def jun26_vae_ac():
                 job_id)
             filename = os.path.join(config_dir, file_name)
             file_paths.append(filename)
-            make_config_file(filename, config)
-    # for ind, job in enumerate(job_list):
-    #     run_python_file(job, file_paths[ind])
+            # make_config_file(filename, config)
+    for ind, job in enumerate(job_list):
+        run_python_file(job, file_paths[ind])
 
 
 if __name__ == "__main__":
