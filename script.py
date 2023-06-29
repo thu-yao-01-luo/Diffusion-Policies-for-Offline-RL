4cf5ee7087ceeb3f0a892a86daa6afc83f9d760a
lkr <2793158317@qq.com>
2023-06-28 20:46:28 -0700

    update

--- script.py
+++ script.py
@@ -1330,6 +1330,46 @@ def jun28_walker():
     for ind, job in enumerate(job_list):
         run_python_file(job, file_paths[ind]) 
 
+def jun28_():
+    file_paths = []
+    job_list = []
+    # env = ["halfcheetah-medium-v2"]
+    env = ["walker2d-medium-v2"]
+    Ts = [1, 2, 4, 8]
+    config_dir = "configs/sample-bc-low/"
+    os.makedirs(config_dir, exist_ok=True)
+    for env_name in env:
+        # for bc_weight in bc_weights:
+        for T in Ts:
+            job_id = f"{env_name[:6]}-ql-bcw-low-t{T}"
+            file_name = job_id + ".yaml"
+            config = {
+                "discount2": 0.999,
+                "coef": 1.0,
+                "seed": 0,
+                "T": T,
+                "algo": "ql",
+                "env_name": env_name,
+                "bc_weight": 0.01,
+                "tune_bc_weight": False,
+                "name": job_id,
+                "id": job_id,
+                "bc_lower_bound": 5e-2,
+                "bc_decay": 0.995,
+                "value_threshold": 2.8e-4,
+                "bc_upper_bound": 1e2,
+                "predict_epsilon": False,   
+                "consistency": False,
+            }
+            job_list.append(
+                job_id)
+            filename = os.path.join(config_dir, file_name)
+            file_paths.append(filename)
+            make_config_file(filename, config)
+    for ind, job in enumerate(job_list):
+        run_python_file(job, file_paths[ind]) 
+
+
 if __name__ == "__main__":
     # jun22_all_env()
     # jun23_discount_all_env()
