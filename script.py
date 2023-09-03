ccac7ea2d2675620fa9078d4f270131dca1793be
lkr <2793158317@qq.com>
2023-09-02 20:31:52 -0700

    update

--- script.py
+++ script.py
@@ -2662,7 +2662,8 @@ def sept3_resample_eval():
 def sept3_dql_param():
     file_paths = []
     job_list = []
-    Ts = [1, 4, 8, 16]
+    # Ts = [1, 4, 8, 16]
+    Ts = [4]
     task_id = f"resample-eval/change{(time.strftime('%H:%M:%S'))}"
     config_dir = f"configs/resample-eval/"
     os.makedirs(config_dir, exist_ok=True)
@@ -2676,7 +2677,8 @@ def sept3_dql_param():
                     "name": job_id, 
                     "id": job_id, 
                     "predict_epsilon": False, 
-                    "format": ['stdout', "wandb", "csv"],
+                    # "format": ['stdout', "wandb", "csv"],
+                    "format": ['stdout', "csv"],
                     "env_name": "halfcheetah-medium-v2", 
                     # "d4rl": True,            
                     # "need_animation": True, 
