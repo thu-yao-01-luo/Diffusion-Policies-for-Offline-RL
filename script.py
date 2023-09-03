6fb09b492e84a8ce11eeac3c8d8933075dae2ac7
lkr <2793158317@qq.com>
2023-09-03 00:33:44 -0700

    fix script and dql

--- script.py
+++ script.py
@@ -2739,6 +2739,44 @@ def sept3_dql_param_with_v():
         os.system("git log -1 -2 -3 > " + git_log)
         run_python_file(job, file_paths[ind], main="experiment.py")
 
+
+def sept3_dql_wv_test_critic():
+    file_paths = []
+    job_list = []
+    Ts = [1, 4, 8, 16]
+    task_id = f"resample-eval/change{(time.strftime('%H:%M:%S'))}"
+    config_dir = f"configs/resample-eval/"
+    os.makedirs(config_dir, exist_ok=True)
+    for T in Ts:
+                job_id = f"dql-with-v-t{T}-oldparam"
+                file_name = job_id + ".yaml"
+                config = {
+                    "algo": "dql", 
+                    "T": T, 
+                    "name": job_id, 
+                    "id": job_id, 
+                    "predict_epsilon": False, 
+                    "format": ['stdout', "wandb", "csv"],
+                    "env_name": "halfcheetah-medium-v2", 
+                    "d4rl": True,            
+                    "online": False,
+                    "num_steps_per_epoch": 5000,
+                    "bc_weight": 1.0,
+                    "num_epochs": 10000,
+                    "test_critic": True,
+                }
+                job_list.append(job_id)
+                filename = os.path.join(config_dir, file_name)
+                file_paths.append(filename)
+                make_config_file(filename, config)
+    for ind, job in enumerate(job_list):
+        dir_path = os.path.join("inter_result", task_id)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
+        git_log = os.path.join(dir_path, "git_log")
+        os.system("git log -1 -2 -3 > " + git_log)
+        run_python_file(job, file_paths[ind], main="experiment.py")
+
 if __name__ == "__main__":
     # jun22_all_env()
     # jun23_discount_all_env()
@@ -2804,3 +2842,4 @@ if __name__ == "__main__":
     # sept3_resample_eval()
     # sept3_dql_param()
     sept3_dql_param_with_v()
+    sept3_dql_wv_test_critic()
