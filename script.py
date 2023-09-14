d7db428fa19478157e6a81b7fb8e2f64ef7d90fd
lkr <2793158317@qq.com>
2023-09-14 01:12:30 -0700

    update

--- script.py
+++ script.py
@@ -4782,6 +4782,56 @@ def sept14_nb_hopbc():
         run_python_file(job, file_paths[ind], main="nb.py")
         # run_multi_py(job, file_paths[ind], main="nb.py", directory=dir_path)
 
+
+def sept14_nb_hopbc():
+    file_paths = []
+    job_list = []
+    task_id = f"sys_test/sept14_nb_hopbc"
+    config_dir = f"configs/sys_test/sept14_nb_hopbc"
+    os.makedirs(config_dir, exist_ok=True)
+    # schedulers = ["ddpm"]
+    # env = ["hopper-medium-v2"]
+    env_name = "hopper-medium-v2"
+    scheduler = "ddpm"
+    # for scheduler in schedulers:
+        # for env_name in env:
+    for T in [1, 4, 8]:
+       for bc_weight in [0.1, 0.2, 0.4, 0.8]: 
+            infer_steps = min(T, 4) 
+            job_id = f"{env_name[:4]}-t{T}-infer{infer_steps}-{scheduler[-4:]}-bc{bc_weight}-sept14-nb-hopbc"
+            file_name = job_id + ".yaml"
+            config = {
+                "predict_epsilon": False, 
+                "format": ['stdout', "wandb", "csv"],
+                "d4rl": True,            
+                "online": False,
+                "num_steps_per_epoch": 5000,
+                "n_inf_steps": infer_steps,
+                "discount2": 1.0,
+                "T": T,
+                "algo": "dac",
+                "env_name": env_name,
+                "bc_weight": bc_weight,
+                "tune_bc_weight": False,
+                "name": job_id,
+                "id": job_id,
+                "sampler_type": scheduler,
+                "vec_env_eval": True,
+            }
+            job_list.append(
+                job_id)
+            filename = os.path.join(config_dir, file_name)
+            file_paths.append(filename)
+            make_config_file(filename, config)
+    for ind, job in enumerate(job_list):
+        dir_path = os.path.join("inter_result", task_id)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
+        git_log = os.path.join(dir_path, "git_log")
+        os.system("git log -1 -2 -3 > " + git_log)
+        # run_python_file(job, file_paths[ind], main="nb.py")
+        run_multi_py(job, file_paths[ind], main="nb.py", directory=dir_path)
+
 if __name__ == "__main__":
     # jun22_all_env()
     # jun23_discount_all_env()
