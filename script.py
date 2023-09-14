17da9384906db51dff8eaa8947c81f97e45c8b97
lkr <2793158317@qq.com>
2023-09-14 01:15:02 -0700

    update

--- script.py
+++ script.py
@@ -4782,7 +4782,7 @@ def sept14_nb_hopbc():
         run_python_file(job, file_paths[ind], main="nb.py")
         # run_multi_py(job, file_paths[ind], main="nb.py", directory=dir_path)
 
-def sept14_nb_hopbc():
+def sept14_nb_hopbc_ql():
     file_paths = []
     job_list = []
     task_id = f"sys_test/sept14_nb_hopbc"
