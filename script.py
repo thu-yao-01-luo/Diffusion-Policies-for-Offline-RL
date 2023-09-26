04adff4ee119398dfa3ba0af93aa1ea412dec3dc
lkr <2793158317@qq.com>
2023-09-26 05:15:20 -0700

    update

--- script.py
+++ script.py
@@ -5993,7 +5993,8 @@ def run_hyper(env_name, hyper, rollout=1, nautilus=True):
     num_epochs = hyper['num_epochs']
     gn = hyper['gn']
     top_k = hyper['top_k']
-    job_id = f"{env_name}-lr{lr}-eta{eta}-maxq{int(max_q_backup)}-{reward_tune[:3]}-eval{eval_freq}-ep{num_epochs}-gn{gn}-top{top_k}-sept26-nb"
+    # job_id = f"{env_name}-lr{lr}-eta{eta}-maxq{int(max_q_backup)}-{reward_tune[:3]}-eval{eval_freq}-ep{num_epochs}-gn{gn}-top{top_k}-sept26-nb"
+    job_id = "f{env_name}-roll{rollout}"
     file_name = job_id + ".yaml"
     config = {
         "predict_epsilon": False, 
